cd88efc4c4f8c29cc39e240509ecc6c004b1b18f
ngoctule12345 <lamem411@gmail.com>
2023-11-23 17:30:58 +0700

    first commit

---
+++ client-customer/src/App.js
@@ -0,0 +1,37 @@
+//CLI: npm install axios --save
+import axios from 'axios';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Main from './components/MainComponent';
+import MyProvider from './contexts/MyProvider';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Loading...'
+    };
+  }
+  render() {
+    return (
+      <MyProvider>
+        <BrowserRouter>
+          <Main>
+          <div>
+            <h2>Customer page</h2>
+            <p>{this.state.message}</p>
+          </div>
+          </Main>
+        </BrowserRouter>
+      </MyProvider>
+    );
+  }
+  componentDidMount() {
+    axios.get('/hello').then((res) => {
+      const result = res.data;
+      this.setState({ message: result.message });
+    });
+  }
+}
+export default App;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.qrmcmwv.mongodb.net',
+    DB_USER: 'dotunglamm',
+    DB_PASS: '0978992802lamtungdo',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'caigicungduoc',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'lamem411@gmail.com', // gmail service
+    EMAIL_PASS: 'licq tamw lxmy tvqx'
+  };
+  module.exports = MyConstants;
